577d1bd8d83cb8f30c32b746c21009be12cb83ef
Gael Porteu de La Morandière <gael.porteu@gmail.com>
2021-03-15 00:25:19 -0400

    Third change descending-order.js

--- descending-order.js
+++ descending-order.js
@@ -1,3 +1,7 @@
 function descendingOrder(n){
-    return Number(n.toString().split('').reverse().join('')); 
+    let reversed = Number(n.toString().split('').reverse().join('')); 
+    console.log(reversed)
   }
+
+  
+  descendingOrder(1234)
