cfc27f21bdb63d361afd1115632cb34d5ee80a28
Gael Porteu de La Morandière <gael.porteu@gmail.com>
2021-03-18 00:43:45 -0400

    Made changes to stray-number

--- stray-number.js
+++ stray-number.js
@@ -1,3 +1,12 @@
 function stray(numbers) {
-    return 0;
+  let strayChar = numbers[0];
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (strayChar != numbers[i]) {
+      return (strayChar = numbers[i]);
+    }
   }
+  return 0;
+}
+
+console.log(stray([]));
