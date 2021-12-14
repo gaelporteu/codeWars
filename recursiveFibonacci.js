fe3c5b221387d18d28a8ee528068a7d6b80bc75f
Gael Porteu de La Morandière <gael.porteu@gmail.com>
2021-12-14 18:29:06 -0500

    Add more info to recursiveFibonacci.js

--- recursiveFibonacci.js
+++ recursiveFibonacci.js
@@ -5,7 +5,10 @@ function fib(num){
   return fib(num-1)+fib(num-2);
 }
 
+console.log(fib(5), 5)
 console.log(fib(6), 8)
+console.log(fib(8), 21)
+console.log(fib(12), 144)
 
 // if (num < 2) { return num; }
 // fib(0) = 0
@@ -13,3 +16,4 @@ console.log(fib(6), 8)
 // fib(2) = 1 -> since 2 is = 2 not not less than 2 --> a recursive call begins
 // need to get to the base case of less than 2
 // fib(3) = 2
+// memoization --> store the results of the recursive calls
